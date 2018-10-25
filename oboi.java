aaf433d1f4b68be604c9eead8d5b65437c7b71cf
VladymyrDiachenko <shantor.s2@gmail.com>
2018-10-25 19:42:32 +0300

    Init commit

---
+++ oboi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class oboi {
+	
+	public static void  main (String[] args ) {
+		int i,j,k,n=5,m=6;
+		for (k=0;k<m;k++) {
+		for (j=0;j<n;j++) {
+		for (i=0;i<4;i++) {
+			System.out.print("+");
+			
+		}
+		for (i=0;i<4;i++) {
+			System.out.print("=");
+			
+		}
+		}
+		System.out.println();
+		
+		}
+	}
+
+}
